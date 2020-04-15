3cac5b05ba8f17307f22639bfb40e103867d236c
George Bentes <george.bentes@gmail.com>
2020-04-15 06:25:32 -0300

    Create new todo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,13 +1,37 @@
-import 'source-map-support/register'
+import 'source-map-support/register';
+import * as uuid from 'uuid';
+import * as AWS from 'aws-sdk';
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest';
+import { TodoItem } from '../../models/TodoItem';
 
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+const todosTable = process.env.TODOS_TABLE;
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
   console.log('newTodo: ', newTodo);
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const todo: TodoItem = {
+    createdAt: new Date().toISOString(),
+    done: false,
+    dueDate: newTodo.dueDate,
+    name: newTodo.name,
+    todoId: uuid.v4(),
+    userId: "1",
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: todo
+  }).promise();
+
+  return {
+    statusCode: 201,
+    headers:{
+      'Access-Control-Allow-Origin':'*'
+    },
+    body: JSON.stringify(todo)
+  }
 }
