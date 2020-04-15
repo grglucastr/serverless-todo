c5eb1a7f496fa1c29b7afc304b57729c5c90b751
George Bentes <george.bentes@gmail.com>
2020-04-15 00:11:28 -0300

    Fetch todos for a hard coded user

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,10 +1,31 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTable = process.env.TODOS_TABLE;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
   console.log('event: ', event);
-  
-  return undefined;
+
+  const params = {
+    TableName : todosTable,
+    KeyConditionExpression: "#uId = :uId",
+    ExpressionAttributeNames:{
+        "#uId": "userId"
+    },
+    ExpressionAttributeValues: {
+        ":uId":"1"
+    }
+  }
+
+  const todosForUser1 = await docClient.query(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(todosForUser1)
+  }
+
 }
