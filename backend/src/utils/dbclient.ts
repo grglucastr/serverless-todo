029189ea6e5676667d16ea6d3476940919100bba
George Bentes <george.bentes@gmail.com>
2020-04-17 02:26:40 -0300

    Change environment variable

--- backend/src/utils/dbclient.ts
+++ backend/src/utils/dbclient.ts
@@ -3,8 +3,8 @@ import * as AWS from 'aws-sdk';
 const databaseEndpoint = process.env.LOCAL_DATABASE_ENDPOINT;
 const databasePort = process.env.LOCAL_DATABASE_PORT;
 
-export default function dbClient() : AWS.DynamoDB.DocumentClient{
-    if(process.env.OFFLINE){
+export const databaseConnection = () : AWS.DynamoDB.DocumentClient => {
+    if(process.env.RUNNING_OFFLINE){
         const databaseURL = `${databaseEndpoint}:${databasePort}`;
         return new AWS.DynamoDB.DocumentClient({
             region: 'localhost',
