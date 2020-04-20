58c0ddfd80717decbfb45c32d9d47e7f06aa6192
George Bentes <george.bentes@gmail.com>
2020-04-19 23:52:32 -0300

    Add X-Ray trace

--- backend/src/utils/dbclient.ts
+++ backend/src/utils/dbclient.ts
@@ -1,15 +1,28 @@
+import * as AWSXRay from 'aws-xray-sdk-core';
 import * as AWS from 'aws-sdk';
+import { DynamoDB } from 'aws-sdk';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+
+
 
 const databaseEndpoint = process.env.LOCAL_DATABASE_ENDPOINT;
 const databasePort = process.env.LOCAL_DATABASE_PORT;
 
+
 export const databaseConnection = () : AWS.DynamoDB.DocumentClient => {
+    let client: DocumentClient;
+    client = new AWS.DynamoDB.DocumentClient();
+
     if(process.env.RUNNING_OFFLINE){
         const databaseURL = `${databaseEndpoint}:${databasePort}`;
-        return new AWS.DynamoDB.DocumentClient({
-            region: 'localhost',
-            endpoint: databaseURL
+        client = new DocumentClient({
+            service: new DynamoDB({
+                region: 'localhost',
+                endpoint: databaseURL
+            })
         });
+        AWSXRay.captureAWSClient((client as any).service);
+        return client;
     }
-    return new AWS.DynamoDB.DocumentClient();
+    return client;
 }
